3e0b0730cd85278a15e432e61e7702cd9cdfc744
Jaroslav Holub <jaroslav@automato.io>
2022-06-08 10:18:36 +0800

    add validations, upgrade dependencies

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/automato-io/terraform-provider-binocs
 go 1.17
 
 require (
-	github.com/automato-io/binocs-client-go v0.0.0-20220603092429-dce96bc0035a
+	github.com/automato-io/binocs-client-go v0.1.0
 	github.com/hashicorp/terraform-plugin-sdk/v2 v2.17.0
 )
 
@@ -16,7 +16,7 @@ require (
 	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-cty v1.4.1-0.20200414143053-d3edf31b6320 // indirect
-	github.com/hashicorp/go-hclog v1.2.0 // indirect
+	github.com/hashicorp/go-hclog v1.2.1 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/go-plugin v1.4.4 // indirect
 	github.com/hashicorp/go-uuid v1.0.3 // indirect
@@ -24,7 +24,7 @@ require (
 	github.com/hashicorp/hcl/v2 v2.12.0 // indirect
 	github.com/hashicorp/logutils v1.0.0 // indirect
 	github.com/hashicorp/terraform-plugin-go v0.9.1 // indirect
-	github.com/hashicorp/terraform-plugin-log v0.4.0 // indirect
+	github.com/hashicorp/terraform-plugin-log v0.4.1 // indirect
 	github.com/hashicorp/terraform-registry-address v0.0.0-20220510144317-d78f4a47ae27 // indirect
 	github.com/hashicorp/terraform-svchost v0.0.0-20200729002733-f050f53b9734 // indirect
 	github.com/hashicorp/yamux v0.0.0-20211028200310-0bc27b27de87 // indirect
@@ -40,7 +40,7 @@ require (
 	github.com/vmihailenco/msgpack/v4 v4.3.12 // indirect
 	github.com/vmihailenco/tagparser v0.1.2 // indirect
 	github.com/zclconf/go-cty v1.10.0 // indirect
-	golang.org/x/net v0.0.0-20220531201128-c960675eff93 // indirect
+	golang.org/x/net v0.0.0-20220607020251-c690dde0001d // indirect
 	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
--- internal/provider/resource_check.go
+++ internal/provider/resource_check.go
@@ -2,10 +2,42 @@ package provider
 
 import (
 	"fmt"
+	"net"
+	"regexp"
+	"strconv"
+	"strings"
 
 	"github.com/automato-io/binocs-client-go"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 )
+
+const (
+	protocolHTTP  = "HTTP"
+	protocolHTTPS = "HTTPS"
+	protocolTCP   = "TCP"
+)
+
+const (
+	validDNSNamePattern = `^([a-zA-Z0-9_]{1}[a-zA-Z0-9_-]{0,62}){1}(\.[a-zA-Z0-9_]{1}[a-zA-Z0-9_-]{0,62})*[\._]?$`
+	validUpCodePattern  = `^([1-5]{1}[0-9]{2}-[1-5]{1}[0-9]{2}|([1-5]{1}(([0-9]{2}|[0-9]{1}x)|xx))){1}(,([1-5]{1}[0-9]{2}-[1-5]{1}[0-9]{2}|([1-5]{1}(([0-9]{2}|[0-9]{1}x)|xx))))*$`
+)
+
+var supportedRegions = []string{
+	"af-south-1",
+	"ap-east-1",
+	"ap-northeast-1",
+	"ap-south-1",
+	"ap-southeast-1",
+	"ap-southeast-2",
+	"eu-central-1",
+	"eu-west-1",
+	"sa-east-1",
+	"us-east-1",
+	"us-west-1",
+}
+
+var supportedHTTPMethods = []string{"GET", "HEAD", "POST", "PUT", "DELETE"}
 
 func checkResource() *schema.Resource {
 	return &schema.Resource{
@@ -23,62 +55,112 @@ func checkResource() *schema.Resource {
 
 		Schema: map[string]*schema.Schema{
 			"name": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Description: "The name (alias) of this check. Maximum length is 25 characters.",
-				Default:     "",
+				Type:         schema.TypeString,
+				Optional:     true,
+				Description:  "The name (alias) of this check. Maximum length is 25 characters.",
+				Default:      "",
+				ValidateFunc: validation.StringLenBetween(0, 25),
 			},
 			"resource": {
 				Type:        schema.TypeString,
 				Required:    true,
 				Description: "The resource to check; a URL in case of a HTTP(S) resource, or HOSTNAME:PORT in case of a TCP resource.",
 				ForceNew:    true,
+				ValidateFunc: func(i interface{}, s string) (_ []string, errors []error) {
+					v, ok := i.(string)
+					if !ok {
+						errors = append(errors, fmt.Errorf("expected type of %q to be string", s))
+						return
+					}
+					if strings.HasPrefix(v, "tcp://") {
+						rc := strings.Split(v[6:], ":")
+						if len(rc) != 2 {
+							errors = append(errors, fmt.Errorf("expected %q tcp resource to contain host and port components", s))
+							return
+						}
+						if !isHost(rc[0]) {
+							errors = append(errors, fmt.Errorf("expected %q tcp resource to contain a valid host", s))
+						}
+						port, err := strconv.Atoi(rc[1])
+						if err != nil {
+							errors = append(errors, fmt.Errorf("expected %q tcp resource to contain a port number", s))
+							return
+						}
+						if _, errs := validation.IsPortNumber(port, s); len(errs) > 0 {
+							errors = append(errors, fmt.Errorf("expected %q tcp resource to contain a valid port number", s))
+						}
+						return
+					}
+					if strings.HasPrefix(v, "http") {
+						_, err := validation.IsURLWithHTTPorHTTPS(v, s)
+						if len(err) > 0 {
+							errors = append(errors, err...)
+						}
+						return
+					}
+					errors = append(errors, fmt.Errorf("expected %q to be either TCP or HTTP(S) resource", s))
+					return
+				},
 			},
 			"method": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Description: "The HTTP method (one of GET, HEAD, POST, PUT or DELETE). Only required for HTTP(S) resources.",
-				// RequiredWith: []string{"protocol=http|https"},
+				Type:         schema.TypeString,
+				Optional:     true,
+				Description:  fmt.Sprintf("The HTTP method (one of %s). Only used and required with HTTP(S) resources.", strings.Join(supportedHTTPMethods, ", ")),
+				ValidateFunc: validation.StringInSlice(supportedHTTPMethods, false),
 			},
 			"interval": {
-				Type:        schema.TypeInt,
-				Optional:    true,
-				Description: "How often Binocs checks this resource, in seconds. Minimum is 5 and maximum is 900 seconds.",
-				Default:     60,
+				Type:         schema.TypeInt,
+				Optional:     true,
+				Description:  "How often Binocs checks this resource, in seconds. Minimum is 5 and maximum is 900 seconds.",
+				Default:      60,
+				ValidateFunc: validation.IntBetween(5, 900),
 			},
 			"target": {
-				Type:        schema.TypeFloat,
-				Optional:    true,
-				Description: "The response time that accommodates Apdex=1.0 (in seconds with up to 3 decimal places). Valid target is a value between 0.01 and 10.0 seconds.",
-				Default:     1.2,
+				Type:         schema.TypeFloat,
+				Optional:     true,
+				Description:  "The response time that accommodates Apdex=1.0 (in seconds with up to 3 decimal places). Valid target is a value between 0.01 and 10.0 seconds.",
+				Default:      1.2,
+				ValidateFunc: validation.FloatBetween(0.01, 10.0),
 			},
 			"regions": {
 				Type:        schema.TypeSet,
-				Optional:    true,
-				Description: "From where in the world Binocs checks this resource.",
+				Required:    true,
+				Description: fmt.Sprintf("From where in the world Binocs checks this resource. At least one region is required. Valid values: %s.", strings.Join(supportedRegions, ", ")),
+				MinItems:    1,
 				Elem: &schema.Schema{
-					Type:     schema.TypeString,
-					Default:  []string{"eu-west-1", "eu-central-1", "us-east-1", "us-west-1", "ap-southeast-1", "ap-northeast-1"},
-					MinItems: 1,
+					Type: schema.TypeString,
+					ValidateFunc: func(i interface{}, s string) (_ []string, errors []error) {
+						v, ok := i.(string)
+						if !ok {
+							errors = append(errors, fmt.Errorf("expected type of %q to be string", s))
+							return
+						}
+						if !stringInSlice(v, supportedRegions) {
+							errors = append(errors, fmt.Errorf("expected %q to be any of %q", "regions", strings.Join(supportedRegions, ", ")))
+						}
+						return
+					},
 				},
 			},
 			"up_codes": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Description: "The good (\"up\") HTTP(S) response codes, e.g. 2xx or `200-302`, or `200,301`",
-				Default:     "200-302",
+				Type:         schema.TypeString,
+				Optional:     true,
+				Description:  "The good (\"up\") HTTP(S) response codes, e.g. 2xx or `200-302`, or `200,301`. Only used with HTTP(S) resources.",
+				ValidateFunc: validation.StringMatch(regexp.MustCompile(validUpCodePattern), fmt.Sprintf("up_codes must be of a format such as %q", "200, 2xx, 200-302, 200,301")),
 			},
 			"up_confirmations_threshold": {
-				Type:        schema.TypeInt,
-				Optional:    true,
-				Description: "How many subsequent \"up\" responses need to occur before Binocs creates an incident and triggers notifications. Minimum is 1 and maximum is 10.",
-				Default:     2,
+				Type:         schema.TypeInt,
+				Optional:     true,
+				Description:  "How many subsequent \"up\" responses need to occur before Binocs creates an incident and triggers notifications. Minimum is 1 and maximum is 10.",
+				Default:      2,
+				ValidateFunc: validation.IntBetween(1, 10),
 			},
 			"down_confirmations_threshold": {
-				Type:        schema.TypeInt,
-				Optional:    true,
-				Description: "How many subsequent \"down\" responses need to occur before Binocs closes an incident and triggers \"recovery\" notifications. Minimum is 1 and maximum is 10.",
-				Default:     2,
+				Type:         schema.TypeInt,
+				Optional:     true,
+				Description:  "How many subsequent \"down\" responses need to occur before Binocs closes an incident and triggers \"recovery\" notifications. Minimum is 1 and maximum is 10.",
+				Default:      2,
+				ValidateFunc: validation.IntBetween(1, 10),
 			},
 		},
 	}
@@ -86,7 +168,11 @@ func checkResource() *schema.Resource {
 
 func checkCreate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*binocs.Client)
-	check, err := client.Checks.Create(constructCheckPayload(d))
+	payload, err := constructCheckPayload(d)
+	if err != nil {
+		return err
+	}
+	check, err := client.Checks.Create(payload)
 	if err != nil {
 		return fmt.Errorf("unable to create Binocs check: %s", err)
 	}
@@ -120,12 +206,20 @@ func checkRead(d *schema.ResourceData, meta interface{}) error {
 
 func checkExists(d *schema.ResourceData, meta interface{}) (bool, error) {
 	err := checkRead(d, meta)
+	if err != nil && strings.Contains(err.Error(), "404") {
+		d.SetId("")
+		return false, nil
+	}
 	return err == nil, err
 }
 
 func checkUpdate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*binocs.Client)
-	err := client.Checks.Update(d.Id(), constructCheckPayload(d))
+	payload, err := constructCheckPayload(d)
+	if err != nil {
+		return err
+	}
+	err = client.Checks.Update(d.Id(), payload)
 	if err != nil {
 		return fmt.Errorf("unable to update Binocs check: %s", err)
 	}
@@ -141,23 +235,28 @@ func checkDelete(d *schema.ResourceData, meta interface{}) error {
 	return nil
 }
 
-func constructCheckPayload(d *schema.ResourceData) binocs.Check {
+func constructCheckPayload(d *schema.ResourceData) (binocs.Check, error) {
 	payload := binocs.Check{}
 
 	if v, ok := d.GetOk("name"); ok {
 		payload.Name = v.(string)
 	}
 
-	if v, ok := d.GetOk("protocol"); ok {
-		payload.Protocol = v.(string)
-	}
-
 	if v, ok := d.GetOk("resource"); ok {
 		payload.Resource = v.(string)
+		payload.Protocol = strings.ToUpper(strings.Split(v.(string), ":")[0])
 	}
 
-	if v, ok := d.GetOk("method"); ok {
-		payload.Method = v.(string)
+	if payload.Protocol == protocolHTTP || payload.Protocol == protocolHTTPS {
+		if v, ok := d.GetOk("method"); ok {
+			payload.Method = v.(string)
+		} else {
+			return payload, fmt.Errorf("expected \"method\" to be one of %s for a %s resource", strings.Join(supportedHTTPMethods, ", "), payload.Protocol)
+		}
+	} else {
+		if _, ok := d.GetOk("method"); ok {
+			return payload, fmt.Errorf("\"method\" cannot be used with a %s resource", payload.Protocol)
+		}
 	}
 
 	if v, ok := d.GetOk("interval"); ok {
@@ -177,8 +276,16 @@ func constructCheckPayload(d *schema.ResourceData) binocs.Check {
 		payload.Regions = stringSlice
 	}
 
-	if v, ok := d.GetOk("up_codes"); ok {
-		payload.UpCodes = v.(string)
+	if payload.Protocol == protocolHTTP || payload.Protocol == protocolHTTPS {
+		if v, ok := d.GetOk("up_codes"); ok {
+			payload.UpCodes = v.(string)
+		} else {
+			return payload, fmt.Errorf("expected \"up_codes\" to be set for a %s resource", payload.Protocol)
+		}
+	} else {
+		if _, ok := d.GetOk("up_codes"); ok {
+			return payload, fmt.Errorf("\"up_codes\" cannot be used with a %s resource", payload.Protocol)
+		}
 	}
 
 	if v, ok := d.GetOk("up_confirmations_threshold"); ok {
@@ -189,5 +296,29 @@ func constructCheckPayload(d *schema.ResourceData) binocs.Check {
 		payload.DownConfirmationsThreshold = v.(int)
 	}
 
-	return payload
+	return payload, nil
+}
+
+func isIP(str string) bool {
+	return net.ParseIP(str) != nil
+}
+
+func isDNSName(str string) bool {
+	if str == "" || len(strings.Replace(str, ".", "", -1)) > 255 {
+		return false
+	}
+	return !isIP(str) && regexp.MustCompile(validDNSNamePattern).MatchString(str)
+}
+
+func isHost(str string) bool {
+	return isIP(str) || isDNSName(str)
+}
+
+func stringInSlice(s string, slice []string) bool {
+	for _, h := range slice {
+		if s == h {
+			return true
+		}
+	}
+	return false
 }
